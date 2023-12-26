d7279a8dd2aaf32d080b82b1cbd3a090f8b32468
Sudhanshu22597 <89680026+Sudhanshu22597@users.noreply.github.com>
2023-12-26 19:34:04 +0530

    sudhi.js

--- sudhi.js
+++ sudhi.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt1 =
-  "Hey Babium......!  <<              we have years of memories <<<                years of loving you. . . .                                                                           > elation that ur text generates. < you are special to me infact everything to me  <<                 i can't imagine u with anyone else  <<                           sorry for my anger and the disrespect                                                     > This will never happen again                << I Love You khushi. . . . .                                                      > will u forgive me? ";
+  "Hey Babium......!  << we have years of memories <<< years of loving you. . . . > elation that ur text generates. < you are special to me infact everything to me  << i can't imagine u with anyone else  << sorry for my anger and the disrespect > This will never happen again << I Love You khushi. . . . . > will u forgive me? ";
 var speed = 55;
 
 typeWriter();
@@ -12,7 +12,7 @@ function typeWriter() {
     else if (txt1.charAt(i) == ">")
       document.getElementById("text1").innerHTML = "";
     else if (txt1.charAt(i) == "|") {
-      $(".bg_heart").css("background-image", "')");
+      $(".bg_heart").css("background-image", "");
     } else document.getElementById("text1").innerHTML += txt1.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
