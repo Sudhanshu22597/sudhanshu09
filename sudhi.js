b8c1d8c6a5654bab109580a7b18f2741eeac769b
Sudhanshu22597 <89680026+Sudhanshu22597@users.noreply.github.com>
2023-12-26 19:05:51 +0530

    sudhi.js

--- sudhi.js
+++ sudhi.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt1 =
-  "Hey Babium......!  <<              .we have years of memories <<<                years of loving you. . . .                                                                           > elation that ur text generates. < you are special to me infact everything to me  <<                 i can't imagine u with anyone else  <<                           sorry for my anger and the disrespect                                                      > i promise these will never happen again                << I Love You khushi. . . . .                                                      > will u forgive me ?                    << ";
+  "Hey Babium......!  <<              we have years of memories <<<                years of loving you. . . .                                                                           > elation that ur text generates. < you are special to me infact everything to me  <<                 i can't imagine u with anyone else  <<                           sorry for my anger and the disrespect                                                     > This will never happen again                << I Love You khushi. . . . .                                                      > will u forgive me? ";
 var speed = 55;
 
 typeWriter();
